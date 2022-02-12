3402cd9ae663c14226f8ff996006b4f6557d0787
Randy Apuzzo <apuzzo@gmail.com>
2022-02-12 00:34:15 -0500

    homepage work

--- components/DefaultHomepage.vue
+++ components/DefaultHomepage.vue
@@ -9,12 +9,21 @@
 
       </div>
     </section>
+    <div class="columns">
+      <div class="column is-7 p-6">
+        <h1 class="title is-1 has-text-white">Start getting weekly updates on your Magic the Gathering collection!</h1>
+        <p class="subtitle has-text-grey-lighter has-shadow is-4">Free membership gives you access to digital binder collection tools and user-only pricing features.</p>
+      </div>
+      <div class="column is-5">
 
+        <UserSignUpForm  class="mt-6" />
+      </div>
+    </div>
     <tiles>
       <div class="card">
           <div class="cardContent m-3">
             <h2 class="title">
-            Collection tracking focused on financials
+            Collection tracking focused on financials!
             </h2>
           </div>
       </div>
@@ -34,6 +43,7 @@
 
 <script>
 import Tiles from '@/components/Tiles'
+import UserSignUpForm from './blocks/UserSignUpForm'
 
 export default {
   name: 'DefaultHomepage',
@@ -43,7 +53,8 @@ export default {
     }
   },
   components: {
-      Tiles
+      Tiles,
+      UserSignUpForm
     },
   props: {
     icon: {
---
+++ components/blocks/UserSignUpForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="has-background-black is-rounded p-5 mr-6">
+        <h3 class="is-size-4 has-text-info has-text-weight-bold">Make an Account</h3>
+        <b-field>
+          <div class="control">
+              <label class="label has-text-grey-lighter">User handle</label>
+            <b-input placeholder="User handle (custom validation for only lowercase)"
+              type="text"
+              required
+              validation-message="Only lowercase is allowed"
+              pattern="[a-z]*">
+            </b-input>
+            </div>
+        </b-field>
+
+        <b-field>
+           <div class="control">
+              <label class="label has-text-grey-lighter">Email Address</label>
+              <b-input placeholder="Email" type="email"></b-input>
+            </div>
+
+        </b-field>
+
+
+
+        <b-field>
+          <div class="control">
+              <label class="label has-text-grey-lighter">Password</label>
+
+            <b-input type="password"
+                placeholder="Password reveal input"
+                password-reveal>
+            </b-input>
+            <p class="help has-text-grey-lighter">Must contain one letter, one number, and be at least 6 characters.</p>
+            </div>
+        </b-field>
+    </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'AsideMenu',
+  components: { },
+  props: {
+  }
+}
+</script>
