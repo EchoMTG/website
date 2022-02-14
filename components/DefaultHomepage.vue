0a4a00e9865d1c48a08743a6a1459497188b3e89
Randy Apuzzo <apuzzo@gmail.com>
2022-02-14 00:04:42 -0500

    homepage work

--- components/DefaultHomepage.vue
+++ components/DefaultHomepage.vue
@@ -10,7 +10,7 @@
       </div>
     </section>
     <div class="columns">
-      <div class="column is-7 p-6">
+      <div class="column is-7 p-6 echo-header-text">
         <h1 class="title is-1 has-text-white">Start getting weekly updates on your Magic the Gathering collection!</h1>
         <p class="subtitle has-text-grey-lighter has-shadow is-4">Free membership gives you access to digital binder collection tools and user-only pricing features.</p>
       </div>
@@ -31,16 +31,34 @@
           <div class="cardContent m-3">
 
               <h2 class="title">
-              Appreciation calculated from price acquired
+
               </h2>
 
+          </div>
+      </div>
+      <div class="card">
+          <div class="cardContent m-3">
+            <h2 class="title">
+              7-day Gain/Loss Calculations
+            </h2>
+          </div>
+      </div>
+      <div class="card">
+          <div class="cardContent m-3">
+            <h2 class="title">
+            Weekly Automated Assessment Emails
+            </h2>
           </div>
       </div>
     </tiles>
 
   </div>
 </template>
-
+<style scoped>
+.echo-header-text{
+  text-shadow: 0 0 4px black, 0 0 10px black,0 0 30px black;
+}
+</style>
 <script>
 import Tiles from '@/components/Tiles'
 import UserSignUpForm from './blocks/UserSignUpForm'
