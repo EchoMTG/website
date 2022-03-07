b02fe829dd272bf9bb4a010520563ec6a834f050
Randy Apuzzo <apuzzo@gmail.com>
2022-03-07 00:04:21 -0500

    dashboard work

--- components/UserDashboard.vue
+++ components/UserDashboard.vue
@@ -1,70 +1,53 @@
 <template>
   <span>
-      <!--
-        acquired_value: 25133.21
-change_value: 57.85
-currency_symbol: "$"
-current_high_value_value: 35802.74
-current_value: 39673.05
-current_value_low: 35741.03
-current_value_market: "32418.84"
-packs_value: "0.00"
-percentage_html: "<div class=\"percentage green up\">57.85%</div>"
-sealed_value: "4772.69"
-total_cards: "1660"
-total_common: "732"
-total_foils: "206"
-total_items: "1667"
-total_mythic: "116"
-total_nonfoils: "1461"
-total_packs: "0"
-total_profit: 14539.84
-total_rare: "490"
-total_sealed: "7"
-total_uncommon: "263"
-        -->
+
       <title-bar :title-stack="titleStack" />
       <hero-bar-main />
+
       <section class="section is-main-section">
+
         <tiles>
           <card-widget
             class="tile is-child"
             type="is-primary"
-            icon="account-multiple"
-            :number="stats.total_items"
-            :previous-number="384"
-            previous-period="July, 2019"
-            label="Items Tracked"
+            icon="currency"
+            prefix="$"
+            :number="parseFloat(stats.current_value_market)"
+            :previous-number="parseInt(stats.total_items)"
+            previous-period=" Tracked Items"
+            label="Collection Value"
+          />
+          <card-widget
+            class="tile is-child"
+            type="is-primary"
+            icon="USD"
+            :number="this.totalCardsValue"
+            prefix="$"
+            :previous-number="parseInt(totalCards)"
+            previous-period="Individual Cards"
+            label="Individual Card Value"
+          />
+          <card-widget
+            class="tile is-child"
+            type="is-primary"
+            icon="usd"
+            :number="parseFloat(stats.sealed_value)"
+            prefix="$"
+            previous-period="Sealed Items"
+            :previous-number="parseInt(stats.total_sealed)"
+            label="Sealed Value"
           />
           <card-widget
             class="tile is-child"
             type="is-info"
             icon="usd"
-            :number="stats.current_value"
-            :previous-number="stats.change_value"
-            previous-period="last 7 days"
-            prefix="$"
-            label="Sales"
-          />
-          <card-widget
-            class="tile is-child"
-            type="is-success"
-            icon="chart-timeline-variant"
-            :number="256"
-            :previous-number="384"
-            previous-period="July, 2019"
+            :number="stats.change_value"
+            previous-number=""
+            previous-period="Last 7 days"
             suffix="%"
             label="Performance"
           />
-          <card-widget
-            class="tile is-child"
-            type="is-danger"
-            icon="bell"
-            :number="32"
-            :previous-number="64"
-            label="Alerts"
-            previous-period="July, 2019"
-          />
+
         </tiles>
 
         <card-component
@@ -103,44 +86,7 @@ total_uncommon: "263"
           </div>
         </div>
 
-        <card-component
-          title="Clients"
-          icon="account-multiple"
-          class="has-table has-mobile-sort-spaced"
-          :has-button-slot="true"
-        >
-          <refresh-button slot="button" />
-          <card-toolbar slot="toolbar">
-            <div slot="left" class="buttons has-addons">
-              <button class="button is-active" @click="actionSample">
-                Active
-              </button>
-              <button class="button" disabled>
-                Recent
-              </button>
-              <button class="button" disabled>
-                Archived
-              </button>
-            </div>
-            <form slot="right" @submit.prevent="actionSample">
-              <div class="field has-addons">
-                <div class="control">
-                  <input
-                    class="input"
-                    type="text"
-                    placeholder="Sample field..."
-                  >
-                </div>
-                <div class="control">
-                  <button type="submit" class="button is-primary">
-                    <b-icon icon="dots-horizontal" custom-size="default" />
-                  </button>
-                </div>
-              </div>
-            </form>
-          </card-toolbar>
-          <clients-table-sample data-url="/data-sources/clients.json" />
-        </card-component>
+
       </section>
   </span>
 </template>
@@ -196,10 +142,24 @@ import RefreshButton from '@/components/RefreshButton'
         title: 'Dashboard — EchoMTG'
       }
     },
+    watch: {
+      'stats.total_cards'(newValue) {
+         console.log(newValue)
+      }
+    },
     computed: {
       titleStack () {
         return ['Admin', 'Dashboard']
       },
+      totalCards () {
+
+        return parseInt(this.stats.total_cards)
+      },
+      totalCardsValue(){
+        let val = parseInt(this.stats.current_value_market) - parseInt(this.stats.sealed_value)
+        console.log(val)
+        return val
+      }
     },
     async fetch() {
       let token = getCookie('token')
@@ -209,8 +169,10 @@ import RefreshButton from '@/components/RefreshButton'
         url
       ).then(res => res.json())
       this.stats = this.stats.stats
+      console.log('stats',this.stats)
 
     },
+    fetchOnServer: true,
     methods: {
       randomChartData (n) {
         const data = []
