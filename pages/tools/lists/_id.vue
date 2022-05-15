4a6ceeb44354f9f1f77a9597331cc3c9f6e158fa
Shishir <shishir0019@gmail.com>
2022-05-16 00:28:32 +0600

    list-table-vue

---
+++ pages/tools/lists/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ id }}</div>
+</template>
+
+<script>
+  export default {
+    async asyncData({ params }) {
+      const id = params.id // When calling /abc the slug will be "abc"
+      return { id }
+    }
+  }
+</script>
+
+<style>
+
+</style>
