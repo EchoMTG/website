23274db2e080ffb77181572974bfc2a9ee8bb05d
Randy Apuzzo <apuzzo@gmail.com>
2022-11-01 15:15:59 -0400

    inventory components

---
+++ components/inventory/DeleteInventoryButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-button icon-left="delete" size="is-small" @click="deleteItem()" />
+</template>
+<script>
+
+export default {
+ name: 'DeleteInventoryButton',
+ props: {
+  callback: {
+    type: Function,
+    required: true
+  },
+  inventory_id: {
+    type: Number,
+    required: true
+  }
+ },
+ methods: {
+  deleteItem() {
+
+    this.$echomtg.inventoryDeleteItem(this.inventory_id)
+    if(this.callback){
+      this.callback()
+    }
+  }
+ }
+}
+</script>
