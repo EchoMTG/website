4f1cac697574967f5d9ba22d675b000f2d98bdab
Randy Apuzzo <apuzzo@gmail.com>
2022-12-18 09:40:51 -0500

    billing user complete

--- pages/user/billing.vue
+++ pages/user/billing.vue
@@ -33,32 +33,34 @@
               <b-button @click="isCardModalActive = true">Add Card</b-button>
             </card-component>
           </tiles>
+          <div class="card">
+            <div class="content p-3 has-text-centered">
+              <b-icon icon="help-circle" class="is-pulled-left" /> For billing inquires, please email <a href="mailto:iona@echomtg.com">iona@echomtg.com</a>
+            </div>
+          </div>
+          <!-- Payment Table -->
           <card-component title="Payment History" icon="receipt-text-check" class="tile is-child">
-              <b-table sortable="true" searchable="true" v-if="cards.length > 0" :data="payments">
-                <b-table-column field="statement_description" label="Description"   v-slot="props">
+              <b-table default-sort-direction="DESC" default-sort="created" v-if="cards.length > 0" :data="payments">
+                <b-table-column field="statement_description" label="Description" sortable v-slot="props">
                   <span :title="props.row.id">{{props.row.statement_description}}</span>
                 </b-table-column>
-                <b-table-column field="created" numeric label="Payment Date"   v-slot="props">
+                <b-table-column field="created" numeric label="Payment Date" sortable v-slot="props">
                 {{getDateFromUnixTimestamp(props.row.created)}}
                 </b-table-column>
-                <b-table-column field="amount" label="Amount"   v-slot="props">
+                <b-table-column field="amount" label="Amount" sortable  v-slot="props">
                   ${{props.row.amount}}
                   <span class="has-text-danger" v-if="props.row.outcome.reason">{{props.row.outcome.reason}}</span>
                   <span class="has-text-success" v-if="!props.row.outcome.reason">{{props.row.outcome.seller_message}}</span>
                 </b-table-column>
-                <b-table-column field="card.id" label="Card"   v-slot="props">
+                <b-table-column field="card.id" label="Card"  v-slot="props">
                   {{props.row.card.brand}} {{props.row.card.last4}} {{props.row.card.exp_month}}/{{props.row.card.exp_year}}
                 </b-table-column>
-
-
-
-
-                <b-table-column field="row.id" label="Transaction ID"   v-slot="props">
+                <b-table-column field="row.id" label="Transaction ID"  v-slot="props">
                 {{props.row.id}}
                 </b-table-column>
-
               </b-table>
             </card-component>
+            <!-- / Payment Table -->
         </div>
       </div>
       <b-modal v-model="isCardModalActive" :width="640" scroll="keep">
@@ -69,30 +71,59 @@
                   </p>
                 </header>
                 <div class="card-content">
+                  <div class="columns">
+                    <div class="column is-8">
+                      <div class="content">
+                        <p>Billing information is not stored in EchoMTG.
+                          The <a href="https://en.wikipedia.org/wiki/Payment_Card_Industry_Data_Security_Standard" target="_blan">PCI Compliant</a> system <a target="_blank" href="https://www.stripe.com">Stripe</a>
+                           handles transactions and storage. EchoMTG references your info in Stripe with safe hashes.
+                          </p>
+                      </div>
+                    </div>
+                    <div class="column is-4">
+                      <b-image style="width:100%" lazy src="https://assets.echomtg.com/interface/visa-mastercard-discover-amex.png" rounded responsive />
+                    </div>
+                  </div>
+
                   <b-field label="Card Number">
                     <b-input v-model="card_number" name="card_number" />
                   </b-field>
-                  <nav class="level">
-                    <div class="level-item">
-                      <b-field label="Exp Month">
-                        <b-input v-model="card_exp_month" name="card_exp_month" />
+                  <div class="columns">
+                    <div class="column is-4">
+                      <b-field label="Expiration Month">
+                        <b-select v-model="card_exp_month" placeholder="Select a default Sort Option">
+                              <option
+                                  v-for="option in generateMonths"
+                                  :value="option.numeric"
+                                  :key="option.numeric">
+                                  {{ option.name }} ({{ option.numeric }})
+                              </option>
+                          </b-select>
+
                       </b-field>
                     </div>
-                    <div class="level-item">
-                      <b-field label="Exp Year">
-                        <b-input v-model="card_exp_year" name="card_exp_year" />
+                    <div class="column is-4">
+                      <b-field label="Expiration Year">
+                        <b-select v-model="card_exp_year" placeholder="Select a default Sort Option">
+                              <option
+                                  v-for="option in generateYears"
+                                  :value="option"
+                                  :key="option">
+                                  {{ option }}
+                              </option>
+                          </b-select>
                       </b-field>
                     </div>
-                    <div class="level-item">
-                      <b-field label="CVC">
-                        <b-input  v-model="card_cvc" name="card_cvc" />
+                    <div class="column is-4">
+                      <b-field label="CVC Code">
+                        <b-input maxlength="4" minlength="3" v-model="card_cvc"  />
                       </b-field>
                     </div>
-                  </nav>
+                  </div>
                 </div>
                 <footer class="card-footer">
-                  <a @click="isCardModalActive = false" class="card-footer-item">Cancel</a>
-                  <a @click="addCard()" class="card-footer-item">Add Card</a>
+                  <a @click="isCardModalActive = false" class="card-footer-item has-background-white has-text-gray">Cancel</a>
+                  <a @click="addCard()" class="card-footer-item has-background-success has-text-white">Add Card</a>
 
                 </footer>
             </div>
@@ -136,8 +167,8 @@ export default {
       payments: [],
       card_number: '',
       card_cvc: '',
-      card_exp_month: '',
-      card_exp_year: '',
+      card_exp_month: "01",
+      card_exp_year: 2022,
       isCardModalActive: false
     }
   },
@@ -194,6 +225,29 @@ export default {
   computed: {
     titleStack () {
       return ['My Account', 'Billing']
+    },
+    generateYears() {
+      let year = new Date().getFullYear();
+      let totalYears = 15;
+      let yearArray = [year]
+      for(let i=1; i < totalYears; i++){
+        yearArray.push(year+i);
+      }
+      return yearArray;
+    },
+    generateMonths(){
+      let monthNames = [ "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+"JUL", "AUG", "SEPT", "OCT", "NOV", "DEC" ];
+      let monthArray = []
+      let strMonth = ''
+      for(let i=1; i <= 12; i++){
+        strMonth = i < 10 ? `0${i}` : `${i}`
+        monthArray.push({
+          name: monthNames[i-1],
+          numeric: strMonth,
+        });
+      }
+      return monthArray;
     },
     paymentsColumns () {
       return [
