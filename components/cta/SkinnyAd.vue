1e762656a9aa07d3951359d9953f65daa6b069da
Randy Apuzzo <apuzzo@gmail.com>
2023-08-22 16:49:26 -0400

    skinny add

---
+++ components/cta/SkinnyAd.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="skinnyAd">
+    <div class="columns">
+      <div class="column is-7">
+        <h3>{{title}}</h3>
+        <p>{{description}}</p>
+      </div>
+      <div class="column">
+        <create-account-modal />
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+  .skinnyAd{
+    background: pink
+  }
+</style>
+<script>
+import CreateAccountModal from '@/components/user/CreateAccountModal.vue'
+
+export default {
+  name: 'SkinnyAd',
+  components: {
+    CreateAccountModal
+  },
+  props: {
+    callback: {
+      type: Function
+    },
+    title: {
+      type: String,
+      default: 'Create an Account to Access this Feature'
+    },
+    description: {
+      type: String,
+      default: 'Free to Signup Today'
+    }
+
+  }
+}
+</script>
