134bb2edcf7fb78bc5259f952a7f4dd28eb544a2
Randy Apuzzo <apuzzo@gmail.com>
2023-10-03 18:48:53 -0400

    app id

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'apple-itunes-app', content: 'app-id=864697496' },
       { 'http-equiv' :"Permissions-Policy",  content: "interest-cohort=()" }
 
     ],
