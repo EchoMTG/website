70483373129574237650e84d6fedb36095489e32
Randy Apuzzo <apuzzo@gmail.com>
2023-10-07 00:22:14 -0400

    visual and sitemap

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <footer v-show="!authenticated" class="footer has-background-black">
+  <footer v-show="!authenticated" class="footer has-background-black mt-6 py-3">
     <div class="container-fluid">
       <div class="level">
         <div class="level-left">
-          <div class="level-item">
-            &copy; {{ year }}, EchoMTG, ThoughtBomb Studios, LLC.&nbsp;<echo-link class="has-text-grey" url="/legal/privacy-policy/">Privacy Policy</echo-link>&nbsp;and&nbsp;<echo-link class="has-text-grey"  url="/legal/terms-and-conditions/">Terms and Conditions</echo-link>
+          <div class="level-item has-text-grey is-size-7">
+            <p>EchoMTG &copy;{{ year }}, ThoughtBomb Studios, LLC.&nbsp;<echo-link class="has-text-grey-light is-underlined" url="/legal/privacy-policy/">Privacy Policy</echo-link>&nbsp;and&nbsp;<echo-link class="has-text-grey-light  is-underlined"  url="/legal/terms-and-conditions/">Terms and Conditions</echo-link>. Magic: the Gathering™ references are property of Wizards of the Coast. Lorcana references are property of Disney.</p>
           </div>
         </div>
         <div class="level-right">
