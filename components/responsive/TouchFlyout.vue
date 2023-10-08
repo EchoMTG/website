a8ff6867ddf76f39b4297d06bd48c54bf56563bd
Randy Apuzzo <apuzzo@gmail.com>
2023-10-08 09:49:14 -0400

    inventory mobiel responvie work

---
+++ components/responsive/TouchFlyout.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="touch-flyout-ux">
+  <b-button size="is-small" class="is-hidden-desktop" icon-left="dots-vertical" @click="this.open = !this.open" />
+  <div :class="`touch-flyout ${flyoutClass}`">
+    <slot></slot>
+  </div>
+</div>
+</template>
+<script>
+
+export default {
+  name: 'TouchFlyout',
+  data: () => {
+    return {
+      open: false
+    }
+  },
+  computed: {
+    flyoutClass() {
+      return this.open ? 'flyout-open' : 'flyout-closed'
+    }
+  }
+}
+</script>
