8e9ebc0f64bb38c2714d4393451bb3197c12a161
Randy Apuzzo <apuzzo@gmail.com>
2023-10-24 15:40:32 -0400

    Deck visual Mode improvement

---
+++ components/decks/VisualDeckMode.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="visual-deck-view is-relative">
+
+    <div class="columns is-multiline ">
+      <div class="column is-three-quarters pl-5">
+        <h3 class="title is-size-6 mt-2 mb-2 has-text-weight-light">Main Deck ({{mainCount}})</h3>
+        <div class="columns is-multiline is-gapless">
+          <template v-for="row in cmcs">
+            <div class="column" v-bind:key="`row-${row.cmc}`" v-if="getCardsByManaCost(row.cmc,row.type).length > 0">
+                <div
+                  is="visual-deck-card"
+                  v-on:moveToSideboard="moveToSideboard"
+                  v-on:addCard="addCard"
+                  v-on:removeCard="removeCard"
+                  v-for="(item, index) in getCardsByManaCost(row.cmc,row.type)"
+                  v-bind:card="list.card_list[item.reference_id]"
+                  v-bind:cardInfo="item"
+                  v-bind:listid="list.id"
+                  v-bind:callback="callback"
+                  v-bind:sideboard="0"
+                  :iindex="index"
+                  :key="`visual-deck-card-main-${index}`"
+                ></div>
+            </div>
+          </template>
+          <div class="column">
+            <div
+                is="visual-deck-card"
+                v-on:moveToSideboard="moveToSideboard"
+                v-on:addCard="addCard"
+                v-on:removeCard="removeCard"
+                v-for="(item, index) in mainFiltersItemsLands"
+                v-bind:card="list.card_list[item.reference_id]"
+                v-bind:cardInfo="item"
+                v-bind:listid="list.id"
+                v-bind:callback="callback"
+                v-bind:sideboard="0"
+                :iindex="index"
+                :key="`visual-deck-card-main-${index}`"
+                ></div>
+          </div>
+        </div>
+      </div>
+      <div :class="`column is-one-quarter pr-5 ` + (isDarkModeActive ? 'has-background-black' : 'has-background-grey-light')" >
+        <h3 class="title is-size-6 mb-2 mt-2 has-text-weight-light">Sideboard ({{sidebarCount}})</h3>
+          <div
+            is="visual-deck-card"
+            v-on:moveToSideboard="moveToSideboard"
+            v-on:addCard="addCard"
+            v-on:removeCard="removeCard"
+            v-for="(item, index) in sideboardCards"
+            v-bind:card="list.card_list[item.reference_id]"
+            v-bind:cardInfo="item"
+            v-bind:listid="list.id"
+            v-bind:callback="callback"
+            :iindex="index"
+            :key="`visual-deck-card-side-${index}`"
+            v-bind:sideboard="1" ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VisualDeckCard from './VisualDeckCard.vue';
+import { mapState } from 'vuex';
+import Discord from '~/pages/about/discord.vue';
+
+export default {
+  name: 'visual-deck-mode',
+  components: { VisualDeckCard },
+  props: {
+
+    list: {
+      type: Object,
+      required: true
+    },
+    callback: {
+      type: Function,
+      required: true
+    }
+
+  },
+  mounted(){
+
+  },
+  computed: {
+    ...mapState([
+      'isDarkModeActive'
+    ]),
+    mainCount(){
+      let count = 0;
+      for(let i = 0; i < this.mainFiltersItems.length; i++){
+        count += parseInt(this.mainFiltersItems[i].main);
+      }
+      return count;
+    },
+
+    sidebarCount(){
+      let count = 0;
+      for(let i = 0; i < this.sideboardCards.length; i++){
+        count += parseInt(this.sideboardCards[i].side);
+      }
+      return count;
+    },
+    sideboardCards(){
+      return Object.values(this.list.sideboard.grouped_list)
+    },
+    mainFiltersItems(){
+      let mainItems = Object.values(this.list.main.grouped_list)
+      for(let i = 0; i < mainItems.length; i++){
+        mainItems[i].item = this.list.card_list[mainItems[i].reference_id];
+      }
+      return mainItems;
+    },
+    mainFiltersItemsNonLands(){
+
+      return this.mainFiltersItems.filter((row) => !row.item.main_type.toLowerCase().includes('land'))
+    },
+    mainFiltersItemsLands(){
+
+      return this.mainFiltersItems.filter((row) => row.item.main_type.toLowerCase().includes('land'))
+    }
+  },
+  data: function data() {
+    return {
+      orderBy: 'type',
+      cmcs: [
+        {
+          cmc:2,
+          type: 'lessthan'
+        },
+        {
+          cmc:2,
+          type: 'equals'
+        },
+         {
+          cmc:3,
+          type: 'equals'
+        },
+         {
+          cmc:3,
+          type: 'greaterthan'
+        }
+
+      ],
+      orderByOptions: ['type', 'cmc', 'color'],
+      orderByOptionsLabels: {
+        type: 'Spell Type',
+        cmc: 'Mana Cost',
+        color: 'Casting Color',
+      },
+      typeArray: [
+        'creatures',
+        'planeswalkers',
+        'artifacts',
+        'instants',
+        'sorceries',
+        'enchantments',
+        'lands',
+        'tribals',
+      ],
+      typeMap: {
+        creatures: 'Creature',
+        planeswalkers: 'Planeswalker',
+        artifacts: 'Artifact',
+        instants: 'Instant',
+        sorceries: 'Sorcery',
+        enchantments: 'Enchantment',
+        lands: 'Land',
+        tribals: 'Tribal',
+      },
+      colorArray: [
+        'multicolor',
+        'black',
+        'blue',
+        'green',
+        'red',
+        'white',
+        'colorless',
+        'lands',
+      ],
+    }
+  },
+  methods: {
+    getCardsByManaCost(cmc=0,logic='equals'){
+      if(logic == 'greaterthan'){
+        return this.mainFiltersItemsNonLands.filter((item) => parseInt(item.item.cmc) > cmc)
+      }
+      if(logic == 'lessthan'){
+        return this.mainFiltersItemsNonLands.filter((item) => parseInt(item.item.cmc) < cmc)
+      }
+      return this.mainFiltersItemsNonLands.filter((item) => parseInt(item.item.cmc) == cmc)
+    },
+    removeCard: function (itemid) {
+      this.$emit('removeCard', itemid)
+    },
+    addCard: function (mid) {
+      this.$emit('addCard', mid)
+    },
+    moveToSideboard: function (iid) {
+      this.$emit('moveToSideboard', iid)
+    },
+    changeOrder(event, newOrder) {
+      this.orderBy = newOrder
+
+      document.querySelectorAll('ul.deck-tabs li').forEach((el) => {
+        el.classList.remove('is-active')
+      })
+
+      event.target.parentElement.classList.add('is-active')
+    },
+  },
+}
+</script>
+
+<style>
+</style>
