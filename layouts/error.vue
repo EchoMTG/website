afd92c3053d7b227d2882a806d135aca8191e029
Randy Apuzzo <apuzzo@gmail.com>
2023-11-14 15:08:48 -0500

    error checking for bad item page, conver item to fetch from async

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLayout name="404">
+    <div>
+      <div class="text-4xl">You've Arrived Here on Error, boss</div>
+      {{error}}
+      <button class="font-bold button" @click="goBack">Back</button>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  props: ['error'],
+  methods: {
+    goBack() {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  padding: 4px 6px;
+  margin: 10px 0px;
+  background: black;
+  color: white;
+}
+</style>
